53c357ed3bba01fef61ff4d730bdb9324fe3132b
Manoel Martins <mano.martins29@gmail.com>
2021-09-26 18:18:36 -0300

    feat: add environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: `.env` }),
     MongooseModule.forRoot(
-      'mongodb+srv://solrac:positivo10@cluster0.yaxxb.mongodb.net/test',
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yaxxb.mongodb.net/test`,
     ),
     UsersModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import dotenvFlow = require('dotenv-flow');
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  dotenvFlow.config();
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000);
 }
